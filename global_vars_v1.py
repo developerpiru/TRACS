79bdca2d81b3a610dd03d8661937f494c8d1c9e5
Piru Perampalam <23018824+developerpiru@users.noreply.github.com>
2020-03-01 19:41:52 -0500

    updated to v1.1.2

--- global_vars_v1.py
+++ global_vars_v1.py
@@ -7,7 +7,8 @@ def init_vars():
         "Initial condition name": "",                   # name of initial condition (day 0)
         "Final condition name": "",                     # name of final condition (day X)
         "Library reference file": "",                   # path to library reference file
-        "FDR": "0.05"                                   # false discovery rate percentage as decimal
+        "FDR": "",                                      # false discovery rate percentage as decimal
+        "CPU cores": ""                                 # number of CPU cores to use
     }
 
     global LIBRARY_READ_PATH, LIST_INITIAL_PATHS, LIST_FINAL_PATHS, LIST_CAS9_NEG_I_PATHS, LIST_CAS9_NEG_F_PATHS
@@ -34,23 +35,23 @@ def init_vars():
 
     global CAS9_TYPE_NAMING
     CAS9_TYPE_NAMING = [
-        "_Cas9-",                                       # tag to add to Cas9 positive files
-        "_Cas9neg-"                                     # tag to add to Cas9 negative files
+        "_Cas9-",                                       # tag to add to Cas9 positive files - used in pre-processing
+        "_Cas9neg-"                                     # tag to add to Cas9 negative files - used in pre-processing
     ]
 
     global COLUMN_NAMES
     COLUMN_NAMES = {
-        "Library": "Library",
-        "Initial": "Initial",
-        "Final": "Final",
-        "Normalized": ".norm",
-        "log2FC": ".log2FC",
-        "average": ".avg",
-        "sgw": ".sgw",
-        "rank": ".rank",
-        "ES": ".ES",
-        "ER": "EnrichmentRatio",
-        "Cas9 pos": "_Cas9-",
-        "Cas9 neg": "_Cas9neg-"
+        "Library": "Library",                           # name for library column (without the .ES) in the self.ES df
+        "Initial": "Initial.ES",                        # name for initial column (with the .ES) in the self.ES df
+        "Final": "Final.ES",                            # name for final column (with the .ES) in the self.ES df
+        "Normalized": ".norm",                          # suffix for normalized values in intermediate dfs
+        "log2FC": ".log2FC",                            # suffix for log2FC values in intermediate dfs
+        "average": ".avg",                              # suffix for average values in intermediate dfs
+        "sgw": ".sgw",                                  # suffix for weighted sgRNA values in intermediate dfs
+        "rank": ".rank",                                # suffix for gene rank values in intermediate dfs
+        "ES": ".ES",                                    # suffix for enrichment scores in final self.ES df
+        "ER": "EnrichmentRatio",                        # name of Enrichment Ratio column in self.ES df
+        "Cas9 pos": "_Cas9-",                           # suffix for Cas9-positive samples - used in TRACS algorithm
+        "Cas9 neg": "_Cas9neg-"                         # suffix for Cas9-negative samples - used in TRACS algorithm
     }
 
